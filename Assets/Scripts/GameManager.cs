5f315303ccbad0aeace9270b4d9f127f09b4f1f9
john <john.demetriou.jd@gmail.com>
2020-05-13 20:37:00 +0300

    Introduce GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private Button _shootButton;
+
+    [SerializeField]
+    private GameObject _needle;
+    private GameObject[] _needles;
+    private float _needleDistance = 1.5f;
+    private int _needleIndex;
+
+    [SerializeField]
+    private int _totalNeedles;
+
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
