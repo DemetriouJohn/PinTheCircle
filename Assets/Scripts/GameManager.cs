6ec2e0015c604ca3d8486582c169cd59a04bba5d
john <john.demetriou.jd@gmail.com>
2020-05-13 20:56:24 +0300

    Introduce GameManager singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,18 +18,41 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void GetButton()
+    {
+        _shootButton = GameObject.FindWithTag("ShootButton").GetComponent<Button>();
+    }
+
+    public void ShootTheNeedle()
+    {
 
     }
 
     // Start is called before the first frame update
     private void Start()
     {
-
+        CreateNeedles();
     }
 
-    // Update is called once per frame
-    private void Update()
+    private void CreateNeedles()
     {
+        _needles = new GameObject[_totalNeedles];
+        Vector3 temp = transform.position;
+        for (int i = 0; i < _needles.Length; i++)
+        {
+            _needles[i] = Instantiate(_needle, temp, Quaternion.identity);
+            temp.y -= _needleDistance;
+        }
+    }
 
+    public void InstantiateNeedle()
+    {
+        Instantiate(_needle, transform.position, Quaternion.identity);
     }
 }
