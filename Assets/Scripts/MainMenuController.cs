a2022594827fff0ccfb878d4dcc9c4bbfb61a094
john <john.demetriou.jd@gmail.com>
2020-12-02 15:55:43 +0200

    MainMenu: Implement Play Game button

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,11 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneNumbers.Gameplay);
+    }
+}
---
+++ Assets/Scripts/SceneNumbers.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts
+{
+    public static class SceneNumbers
+    {
+        public const byte MainMenu = 0;
+        public const byte Gameplay = 1;
+    }
+}
